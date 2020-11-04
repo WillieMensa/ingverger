a62fc41d3bcdf93316bfc810d54e8d66a0dc13bf
Willie <ingverger@gmail.com>
2020-11-04 00:11:31 -0300

    Version 20201103
    Version 20201103

--- menuppal.js
+++ menuppal.js
@@ -20,6 +20,7 @@ function poneMenuPpal() {
 		'		<div class="dropdown-content">' +
 		'			<a href="./temas-rep-graf.html">Material de estudio</a>' +
 		'			<a href="./guias-de-clase.html">Gu&iacute;as de Clase</a>' +
+		'			<a href="./representacion-con-cad.html">Representación asistida con CAD</a>' +
 		'			<a href="./repgrafsincorbata.html">Representaci&#243;n gr&#225;fica sin corbata</a>' +
 		'		</div>' +
 		'	</li>' +
@@ -27,7 +28,7 @@ function poneMenuPpal() {
 		'		<a href="javascript:void(0)" class="dropbtn">Publicaciones</a>' +
 		'		<div class="dropdown-content">' +
 		'			<a href="matematica-recreativa.html">Matemática recreativa</a>' +
-		'			<a href="publicaciones.php">Publicaciones varias</a>' +
+		'			<a href="publicaciones-varias.html">Publicaciones varias</a>' +
 		'			<a href="metodo-intelectual.php">Metodolog&iacute;a intelectual</a>' +
 		'			<a href="calendarios.html">Impresion calendarios y otros</a>' +
 		'		</div>' +
